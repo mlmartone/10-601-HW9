0351c4c9fd066840a246ff165e8a4b045a0c231a
Matt Martone <mmartone@andrew.cmu.edu>
2017-05-02 03:52:33 -0400

    no regularization, tuned gamma, no gender info included

--- code/gradientDescent.m
+++ code/gradientDescent.m
@@ -1,14 +1,15 @@
 function data = gradientDescent(data)
-gamma = .0025;
-numEpochs = 50;
-lambda = 0.005;
-testAmt = 200000
+gamma = .0015;
+numEpochs = 200;
+lambda = 0;%-.02;
+testAmt = size(data.train,1);
 error = zeros(size(data.train,1),1);
 validationError = zeros(size(data.train,1) - testAmt, 1);
 totalError = zeros(numEpochs+1,1);
 for movie = 1:1:size(data.movieMat,1)
-    data.movieMat(movie,:) = data.movieMat(movie,:)/...
-        sum(data.movieMat(movie,:)');
+    data.movieMat(movie,1:size(data.genres,1)-data.numDemos) = ...
+        data.movieMat(movie,1:size(data.genres,1)-data.numDemos)/...
+        sum(data.movieMat(movie,1:size(data.genres,1)-data.numDemos)');
 end
 for sample = 1:1:size(data.train,1)
     %Get the current sample information
@@ -20,7 +21,6 @@ for sample = 1:1:size(data.train,1)
 end
 totalError(1) = rms(error);
 %Iterate through all training examples, updating with gradient descent
-size(data.train,1)
 for epoch = 1:1:numEpochs
     for sample = 1:1:testAmt %size(data.train,1)
         %Get the current sample information
@@ -37,7 +37,7 @@ for epoch = 1:1:numEpochs
         movieChange = -gamma*movieMatGrad;
         data.movieMat(movie,:) = data.movieMat(movie,:) + movieChange;
     end
-    gamma = max(gamma*.9,.00001);
+    gamma = max(gamma*.95,.000001);
     for sample = testAmt:1:size(data.train,1)
         user = data.train(sample,1);
         movie = data.train(sample,2);
@@ -45,9 +45,9 @@ for epoch = 1:1:numEpochs
         validationError(sample - testAmt + 1) = rating - ...
             sum(data.userMat(user,:).*data.movieMat(movie,:));
     end
-    totalError(epoch+1) = rms(validationError);
+    totalError(epoch+1) = rms(error);%validationError);
     plot(0:1:numEpochs,totalError);
-    pause(.01)
+    pause(.01);
 end
 
 end
